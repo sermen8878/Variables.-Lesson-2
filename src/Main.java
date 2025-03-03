1771326eb7e3a9836c87fb4a0f07d223d5559903
serme <Sermen88@yandex.ru>
2025-03-03 23:03:14 +0500

    Task 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int myIntNumber = 40000;
+        byte myByteValue = 22;
+        short myShortValue = 32000;
+        long myLongValue = 123456789L;
+        float myFloatValue = 3.75f;
+        double myDoubleValue = 2.718281828459045;
+
+        System.out.println("Значение переменной myIntNumber с типом int равно " + myIntNumber);
+        System.out.println("Значение переменной myByteValue с типом byte равно " + myByteValue);
+        System.out.println("Значение переменной myShortValue с типом short равно " + myShortValue);
+        System.out.println("Значение переменной myLongValue с типом long равно " + myLongValue);
+        System.out.println("Значение переменной myFloatValue с типом float равно " + myFloatValue);
+        System.out.println("Значение переменной myDoubleValue с типом double равно " + myDoubleValue);
+    }
+}
